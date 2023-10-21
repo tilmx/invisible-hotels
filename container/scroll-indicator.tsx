0c42e92b14e0561f9829e8c204b053f584885da6
Tilman Frick <git@tilman.io>
2023-10-21 23:10:13 +0200

    feat: add scroll indicator

---
+++ container/scroll-indicator.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { Size } from '../components/tokens/size';
+import { Breakpoint, Color } from '../components';
+import styled from '@emotion/styled';
+import { ArrowDown } from 'lucide-react';
+
+const StyledIndicator = styled.div<{ visible: boolean; }>`
+    position: fixed;
+    left: 50%;
+    bottom: ${Size.XL};
+    z-index: 15;
+    height: ${Size.XL};
+    width: ${Size.XL};
+    border-radius: 50%;
+    background: ${Color.Yellow};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: transform .2s;
+    transform: translateX(-50%);
+    box-shadow: 0 0 ${Size.XXL} rgba(0,0,0,0.3);
+
+    ${Breakpoint.Mobile} {
+        bottom: ${Size.L};
+    }
+
+    &:hover {
+        transform: translateX(-50%) scale(1.1);
+    }
+
+    ${props => !props.visible && 'transform: translateX(-50%) scale(0);'}
+`;
+
+export const ScrollIndicator: React.FunctionComponent = () => {
+    const [visible, setVisible] = React.useState(true);
+
+    React.useEffect(() => {
+        window.addEventListener('scroll', listenToScroll);
+        return () => {
+            window.removeEventListener('scroll', listenToScroll);
+        }
+    })
+
+    const listenToScroll = () => {
+        setVisible(window.scrollY < 200)
+    }
+
+    return (
+        <StyledIndicator visible={visible}>
+            <ArrowDown />
+        </StyledIndicator>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Footer } from '../container/footer'
 import { AccentStyle, BackgroundColor, Color, AccentedText, Logo, Text, TextSize } from '../components'
 import { useState } from 'react'
 import { HotelList } from '../container/hotel-list'
+import { ScrollIndicator } from '../container/scroll-indicator'
 
 export default function Home() {
 	const [color, setColor] = useState<string | undefined>();
@@ -17,6 +18,7 @@ export default function Home() {
 			<Wrapper>
 				<Logo />
 				<Text size={TextSize.Huge} serif>are all the <AccentedText color={Color.Blue} accentStyle={AccentStyle.Scribbled} onMouseEnter={() => setColor(Color.Blue)} onMouseOut={() => setColor(undefined)}>lovely</AccentedText>, <AccentedText color={Color.Green} accentStyle={AccentStyle.Circled} onMouseEnter={() => setColor(Color.Green)} onMouseOut={() => setColor(undefined)}>minimalistic</AccentedText> and <AccentedText color={Color.Yellow} accentStyle={AccentStyle.Underlined} onMouseEnter={() => setColor(Color.Yellow)} onMouseOut={() => setColor(undefined)}>fancy</AccentedText> hotels & apartments where we stayed already — or would love to.</Text>
+				<ScrollIndicator />
 			</Wrapper>
 			<HotelList backgroundColor={color} />
 			<Footer />
