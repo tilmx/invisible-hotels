d0c8f7f7f6bc6e918c30e10880bdd4889ad777fb
Tilman Frick <git@tilman.io>
2023-10-23 22:35:52 +0200

    feat: add plausible tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,21 @@ import { css, Global } from '@emotion/react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Color, Theme } from '../components/tokens/colors';
+import PlausibleProvider from 'next-plausible';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <Theme>
-      <Head>
-        <link rel="icon" href="/favicon.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
-        <meta name="language" content="en" />
-      </Head>
-      <Global styles={css`body { margin: 0; background: ${Color.Background}; color: ${Color.Text}; };`} />
-      <Component {...pageProps} />
-    </Theme>
+    <PlausibleProvider domain="invisible-hotels.com">
+      <Theme>
+        <Head>
+          <link rel="icon" href="/favicon.png" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+          <meta name="language" content="en" />
+        </Head>
+        <Global styles={css`body { margin: 0; background: ${Color.Background}; color: ${Color.Text}; };`} />
+        <Component {...pageProps} />
+      </Theme>
+    </PlausibleProvider>
   );
 }
 
