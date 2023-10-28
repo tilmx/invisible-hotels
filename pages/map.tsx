2c6b69c4cb674be29600e6783a5bd21ec9ba5688
Tilman Frick <git@tilman.io>
2023-10-28 19:15:16 +0200

    refactor: simplify map logic

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { useEffect, useRef, useState } from 'react';
-import Script from 'next/script'
 import { Menu } from '../components/menu';
 import { Wrapper } from '../components/wrapper';
 import styled from '@emotion/styled';
@@ -26,41 +25,51 @@ const StyledMenu = styled(Menu)`
     padding-bottom: ${Size.M};
 `;
 
+let loadingMapPromise: Promise<void> | null = null;
+export function loadMap(token: string): Promise<void> {
+    if (loadingMapPromise !== null) {
+        return loadingMapPromise;
+    }
+    loadingMapPromise = new Promise((resolve) => {
+        const scriptTag = document.createElement('script');
+        scriptTag.addEventListener('load', () => {
+            mapkit.init({ authorizationCallback: (done) => done(token) });
+            resolve();
+        }, { once: true });
+        scriptTag.src = 'https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.js';
+        scriptTag.crossOrigin = '';
+        document.head.appendChild(scriptTag);
+    });
+    return loadingMapPromise;
+}
+
 export default function Map() {
     const [map, setMap] = useState<mapkit.Map | null>(null);
     const element = useRef<HTMLDivElement>(null);
+    const mapExists = useRef(false);
 
     useEffect(() => {
-        if (typeof mapkit !== 'undefined') {
-            mapkit.init({
-                authorizationCallback: function (done: any) {
-                    done(process.env.NEXT_PUBLIC_MAPKIT_TOKEN);
-                }
-            });
-            let map = new mapkit.Map('mapContainer', { center: new mapkit.Coordinate(37.334883, -122.008977), showsMapTypeControl: false });
-
-            return (
-                () => map.destroy()
-            )
-        }
+        loadMap(process.env.NEXT_PUBLIC_MAPKIT_TOKEN!).then(() => {
+            if (mapExists.current) return;
+            setMap(new mapkit.Map(element.current!, { center: new mapkit.Coordinate(37.334883, -122.008977), showsMapTypeControl: false }));
+            mapExists.current = true;
+        });
+        return () => {
+            if (map) {
+                map.destroy();
+                mapExists.current = false;
+            }
+        };
     }, [])
 
     return (
         <>
-            <Script
-                src="https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.core.js"
-                crossOrigin=""
-                async
-                data-callback="initMapKit"
-                data-libraries="services,full-map,geojson"
-            />
             <Head>
                 <title>Map | Invisible Hotels</title>
                 <meta name="description" content="Invisible Hotels are all the lovely, minimalistic and fancy hotels & apartments where we stayed already — or would love to." />
             </Head>
             <StyledMenuContainer>
                 <Wrapper>
-
                     <StyledMenu />
                 </Wrapper>
             </StyledMenuContainer>
