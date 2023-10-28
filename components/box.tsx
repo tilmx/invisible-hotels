2e3491ac91afa431148e9834eba6676725c411bf
Tilman Frick <git@tilman.io>
2023-10-28 23:04:32 +0200

    feat: show cookie opt in before map

---
+++ components/box.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled'
+import { Text, TextSize } from './text';
+import { Breakpoint, Color, Size } from './tokens';
+import { FunctionComponent, ReactNode } from 'react';
+import { CookieIcon } from 'lucide-react';
+
+const StyledMessage = styled.div`
+    padding: ${Size.L};
+    max-width: 400px;
+    margin: 0 auto;
+    border-radius: ${Size.M};
+    background: ${Color.Background80};
+    backdrop-filter: blur(${Size.M});
+    box-shadow: 0 ${Size.XS} ${Size.L} ${Color.Shadow}, inset 0 0 0 1px ${Color.Text20};
+    display: flex;
+    flex-direction: column;
+    gap: ${Size.S};
+
+    > svg {
+        display: block;
+        width: ${Size.L};
+        height: ${Size.L};
+    }
+`;
+
+const StyledButtonList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin-top: ${Size.S};
+    gap: ${Size.XXS};
+
+    ${Breakpoint.Mobile} {
+        grid-template-columns: 1fr;
+    }
+`;
+
+export const Box: FunctionComponent<{ title: string; description: string; children?: ReactNode; }> = props => {
+	return (
+		<StyledMessage>
+			<CookieIcon />
+			<Text size={TextSize.Large} bold>{props.title}</Text>
+			<Text size={TextSize.Regular}>{props.description}</Text>
+			<StyledButtonList>
+				{props.children}
+			</StyledButtonList>
+		</StyledMessage>
+	)
+}
--- components/overlay.tsx
+++ components/overlay.tsx
@@ -1,15 +1,12 @@
 import styled from '@emotion/styled';
 import { FunctionComponent, ReactNode } from 'react';
 import { Color } from './tokens/colors'
-import { Size } from './tokens/size'
-import { Breakpoint } from './tokens/breakpoint'
-import { Cookie } from 'lucide-react';
-import { Text, TextSize } from './text';
 import { OutsideClick } from './utils';
 import { Wrapper } from './wrapper';
+import { Box } from './box';
 
 interface OverlayProps {
-    headline: string;
+    title: string;
     description: string;
     children: ReactNode;
     onOutsideClick: () => void;
@@ -28,49 +25,15 @@ const StyledContainer = styled.div`
     background: ${Color.Shadow};
 `;
 
-const StyledMessage = styled.div`
-    padding: ${Size.L};
-    max-width: 400px;
-    margin: 0 auto;
-    border-radius: ${Size.M};
-    background: ${Color.Background80};
-    backdrop-filter: blur(${Size.M});
-    box-shadow: 0 ${Size.XS} ${Size.L} ${Color.Shadow}, inset 0 0 0 1px ${Color.Text20};
-    display: flex;
-    flex-direction: column;
-    gap: ${Size.S};
-
-    > svg {
-        display: block;
-        width: ${Size.L};
-        height: ${Size.L};
-    }
-`;
-
-const StyledButtonList = styled.div`
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    margin-top: ${Size.S};
-    gap: ${Size.XXS};
-
-    ${Breakpoint.Mobile} {
-        grid-template-columns: 1fr;
-    }
-`;
 
 export const Overlay: FunctionComponent<OverlayProps> = props => {
     return (
         <StyledContainer>
             <Wrapper>
                 <OutsideClick onOutsideClick={props.onOutsideClick}>
-                    <StyledMessage>
-                        <Cookie />
-                        <Text size={TextSize.Large} bold>{props.headline}</Text>
-                        <Text size={TextSize.Regular}>{props.description}</Text>
-                        <StyledButtonList>
-                            {props.children}
-                        </StyledButtonList>
-                    </StyledMessage>
+                    <Box title={props.title} description={props.description}>
+                        {props.children}
+                    </Box>
                 </OutsideClick>
             </Wrapper>
         </StyledContainer>
