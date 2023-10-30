2e1f14281e711c66f8077d8045aed57f824a6a62
Tilman Frick <git@tilman.io>
2023-10-30 22:12:05 +0100

    feat: extend hotel detail page

--- pages/hotel/[hotel].tsx
+++ pages/hotel/[hotel].tsx
@@ -7,8 +7,12 @@ import Head from 'next/head';
 import { siteTitle } from '../_app';
 import { Wrapper } from '../../components/wrapper';
 import { Menu } from '../../components/menu';
-import { getVacationTypeColor } from '../../utils';
+import { getVacationTypeColor, getVacationTypeIcon } from '../../utils';
 import { Footer } from '../../components/footer';
+import { Flex, JustifyContent } from '../../components/utils';
+import { Tag } from '../../components/tag';
+import { HotelCard } from '../../components/hotel-card';
+import { Button } from '../../components/button';
 
 const StyledBackground = styled.div<{ color: string; }>`
     background: ${props => props.color}
@@ -49,17 +53,41 @@ const StyledImage = styled(Image)`
     box-shadow: 0 ${Size.M} ${Size.XXXL} ${Color.Shadow};
 `
 
+const StyledSimilarSection = styled.div`
+    padding-top: ${Size.XXXXXL};
+    display: flex;
+    flex-direction: column;
+    gap: ${Size.S};
+`;
+
+const StyledHotelCardWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: ${Size.M};
+    padding-top: ${Size.XXL};
+`;
+
+const StyledHotelCard = styled(HotelCard)`
+    background: ${Color.Background};
+`;
+
 export default function Hotel({ hotel }: { hotel: typeof hotels[number] }) {
+    const similarHotels = hotels.filter(hotelItem => hotelItem.vacationType === hotel.vacationType && hotelItem.id !== hotel.id);
+
     return (
         <StyledBackground color={getVacationTypeColor(hotel.vacationType) || Color.Background}>
             <Head>
-                <title>{hotel.name} | {siteTitle}</title>
+                <title>{hotel.name} — {siteTitle}</title>
             </Head>
             <Wrapper>
                 <Menu />
                 <StyledIntro>
                     <Text size={TextSize.Huge} bold center>{hotel.name}</Text>
                     <Text size={TextSize.SuperLarge} center serif>{hotel.city}, {hotel.country}</Text>
+                    <Flex justifyContent={JustifyContent.Center} gap={Size.XS}>
+                        <Tag icon={getVacationTypeIcon(hotel.vacationType)} label={hotel.vacationType} />
+                        <Tag label={hotel.housingType} />
+                    </Flex>
                 </StyledIntro>
                 <StyledImageContainer multipleImages={(hotel.images?.length || 0) > 1}>
                     {hotel.images?.slice(0, 2).map((image, i) =>
@@ -73,6 +101,27 @@ export default function Hotel({ hotel }: { hotel: typeof hotels[number] }) {
                         />
                     )}
                 </StyledImageContainer>
+            </Wrapper>
+            <Wrapper wide>
+                <StyledSimilarSection>
+                    <Text size={TextSize.SuperLarge} bold center>Similar Hotels</Text>
+                    <Text size={TextSize.Large} serif center>{similarHotels.length} Hotels near {hotel.vacationType} in {hotel.country}</Text>
+                    <StyledHotelCardWrapper>
+                        {similarHotels.slice(0, 3).map((hotel, i) =>
+                            <StyledHotelCard
+                                key={i}
+                                title={hotel.name}
+                                city={hotel.city}
+                                country={hotel.country}
+                                housingType={hotel.housingType}
+                                vacationType={hotel.vacationType}
+                            />
+                        )}
+                    </StyledHotelCardWrapper>
+                    <Flex justifyContent={JustifyContent.Center}>
+                        <Button>Explore all similar hotels</Button>
+                    </Flex>
+                </StyledSimilarSection>
             </Wrapper>
             <Footer />
         </StyledBackground>
