735c0186d8d4df066328ba21fca89064fd399918
Tilman Frick <git@tilman.io>
2023-11-07 00:47:22 +0100

    fix: adjust tag padding on small viewports

--- components/tag.tsx
+++ components/tag.tsx
@@ -17,7 +17,7 @@ const StyledTag = styled.div<{ hasIcon?: boolean; hasLabel?: boolean; }>`
 	${Breakpoint.Mobile} {
 		padding: ${Size.Special6} ${Size.XS};
 		${props => props.hasIcon && `padding-left: ${Size.XXS};`}
-		${props => !props.hasLabel && `padding-right: ${Size.Special6};`}
+		${props => !props.hasLabel && `padding-right: ${Size.XXS};`}
 		border-width: 1.5px;
 		gap: ${Size.XXXS};
 		svg {
