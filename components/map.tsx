8d9a0aaaf79318ff83a9f5a28d889974fcdba73d
Tilman Frick <git@tilman.io>
2023-11-08 00:26:47 +0100

    refactor: move map to separate component

---
+++ components/map.tsx
@@ -0,0 +1,103 @@
+import { FunctionComponent, useEffect, useRef, useState } from "react";
+
+interface MapProps {
+    className?: string;
+    annotations?: ({
+        id: string;
+        name: string;
+        coordinates: { lat: number; long: number };
+        color: string;
+        clusteringIdentifier: string;
+    })[];
+    onAnnotationClick: (id?: string) => void;
+}
+
+let loadingMapPromise: Promise<void> | null = null;
+function loadMap(token: string): Promise<void> {
+    if (loadingMapPromise !== null) {
+        return loadingMapPromise;
+    }
+    loadingMapPromise = new Promise((resolve) => {
+        const scriptTag = document.createElement('script');
+        scriptTag.addEventListener('load', () => {
+            mapkit.init({ authorizationCallback: (done) => done(token) });
+            resolve();
+        }, { once: true });
+        scriptTag.src = 'https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.js';
+        scriptTag.crossOrigin = '';
+        document.head.appendChild(scriptTag);
+    });
+    return loadingMapPromise;
+}
+
+export const Map: FunctionComponent<MapProps> = props => {
+    const [map, setMap] = useState<mapkit.Map | null>(null);
+    const element = useRef<HTMLDivElement>(null);
+    const mapExists = useRef(false);
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+        setDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches)
+    }, []);
+
+    useEffect(() => {
+        if (mapExists.current) return;
+
+        loadMap(process.env.NEXT_PUBLIC_MAPKIT_TOKEN!).then(() => {
+            if (mapExists.current) return;
+
+            setMap(new mapkit.Map(element.current!, {
+                center: new mapkit.Coordinate(53.551086, 9.993682),
+                showsMapTypeControl: false,
+                isRotationEnabled: false,
+                showsPointsOfInterest: false,
+                mapType: 'mutedStandard',
+                annotationForCluster(annotation) {
+                    (annotation as any).color = 'black';
+                    return annotation;
+                },
+                colorScheme: darkMode ? mapkit.Map.ColorSchemes.Dark : mapkit.Map.ColorSchemes.Light,
+                annotations: props.annotations?.map(annotation => {
+                    return new mapkit.MarkerAnnotation(
+                        new mapkit.Coordinate(annotation.coordinates?.lat, annotation.coordinates?.long),
+                        {
+                            title: annotation.name,
+                            color: annotation.color,
+                            clusteringIdentifier: annotation.clusteringIdentifier,
+                            data: { id: annotation.id }
+                        }
+                    )
+                })
+            }));
+            mapExists.current = true;
+        });
+        return () => {
+            if (map) {
+                map.destroy();
+                mapExists.current = false;
+            }
+        };
+    }, [])
+
+    useEffect(() => {
+        if (!map) { return }
+        const listenToSelect = (e: mapkit.EventBase<mapkit.Map> & { annotation?: mapkit.Annotation | undefined }) => {
+            props.onAnnotationClick(e.annotation?.data.id)
+        }
+        const listenToDeselect = () => {
+            props.onAnnotationClick(undefined)
+        }
+        map?.addEventListener('select', listenToSelect)
+        map?.addEventListener('deselect', listenToDeselect)
+        return () => {
+            map.removeEventListener('select', listenToSelect)
+            map?.addEventListener('deselect', listenToDeselect)
+
+        }
+    }, [map])
+
+    return (
+        <div id="mapContainer" ref={element} className={props.className} />
+    )
+}
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Menu } from '../components/menu';
 import { Wrapper } from '../components/wrapper';
 import styled from '@emotion/styled';
@@ -12,9 +12,10 @@ import { Footer } from '../components/footer';
 import { Size } from '../components/tokens/size';
 import { Breakpoint } from '../components/tokens/breakpoint';
 import { Color } from '../components/tokens/colors';
+import { Map as MapComponent } from '../components/map';
 import { siteTitle } from '../data/site';
 
-const StyledMapElement = styled.div`
+const StyledMapElement = styled(MapComponent)`
     height: 100vh;
     width: 100vw;
 `;
@@ -54,100 +55,14 @@ const StyledHotelCard = styled(HotelCard)`
     box-shadow: 0 ${Size.XXS} ${Size.M} ${Color.Shadow};
 `;
 
-let loadingMapPromise: Promise<void> | null = null;
-function loadMap(token: string): Promise<void> {
-    if (loadingMapPromise !== null) {
-        return loadingMapPromise;
-    }
-    loadingMapPromise = new Promise((resolve) => {
-        const scriptTag = document.createElement('script');
-        scriptTag.addEventListener('load', () => {
-            mapkit.init({ authorizationCallback: (done) => done(token) });
-            resolve();
-        }, { once: true });
-        scriptTag.src = 'https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.js';
-        scriptTag.crossOrigin = '';
-        document.head.appendChild(scriptTag);
-    });
-    return loadingMapPromise;
-}
-
 export default function Map() {
     const [mapCookiesAllowed, setMapCookiesAllowed] = useState<boolean | undefined>(undefined);
-
-    const [map, setMap] = useState<mapkit.Map | null>(null);
-    const element = useRef<HTMLDivElement>(null);
-    const mapExists = useRef(false);
-
-    const [darkMode, setDarkMode] = useState(false);
 
     const [selectedHotel, setSelectedHotel] = useState<string | undefined>();
 
     useEffect(() => {
         setMapCookiesAllowed(checkIfCookiesAllowed("map"))
     })
-
-    useEffect(() => {
-        setDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches)
-    }, []);
-
-    useEffect(() => {
-        if (mapExists.current || !mapCookiesAllowed) return;
-
-        loadMap(process.env.NEXT_PUBLIC_MAPKIT_TOKEN!).then(() => {
-            if (mapExists.current) return;
-
-            setMap(new mapkit.Map(element.current!, {
-                center: new mapkit.Coordinate(53.551086, 9.993682),
-                showsMapTypeControl: false,
-                isRotationEnabled: false,
-                showsPointsOfInterest: false,
-                mapType: 'mutedStandard',
-                annotationForCluster(annotation) {
-                    (annotation as any).color = 'black';
-                    return annotation;
-                },
-                colorScheme: darkMode ? mapkit.Map.ColorSchemes.Dark : mapkit.Map.ColorSchemes.Light,
-                annotations: hotelsPreview.filter(hotel => hotel.coordinates)?.map(hotel => {
-                    if (hotel.coordinates.lat && hotel.coordinates.long) {
-                        return new mapkit.MarkerAnnotation(
-                            new mapkit.Coordinate(hotel.coordinates?.lat, hotel.coordinates?.long),
-                            {
-                                title: hotel.name,
-                                color: getVacationTypeColor(hotel.vacationType),
-                                clusteringIdentifier: hotel.country,
-                                data: { id: hotel.id }
-                            }
-                        );
-                    }
-                }) as any
-            }));
-            mapExists.current = true;
-        });
-        return () => {
-            if (map) {
-                map.destroy();
-                mapExists.current = false;
-            }
-        };
-    }, [mapCookiesAllowed])
-
-    useEffect(() => {
-        if (!map) { return }
-        const listenToSelect = (e: mapkit.EventBase<mapkit.Map> & { annotation?: mapkit.Annotation | undefined }) => {
-            setSelectedHotel(e.annotation?.data.id)
-        }
-        const listenToDeselect = () => {
-            setSelectedHotel(undefined)
-        }
-        map?.addEventListener('select', listenToSelect)
-        map?.addEventListener('deselect', listenToDeselect)
-        return () => {
-            map.removeEventListener('select', listenToSelect)
-            map?.addEventListener('deselect', listenToDeselect)
-
-        }
-    }, [map])
 
     const selectedHotelContent = hotelsPreview.find(hotel => hotel.id === selectedHotel)
 
@@ -173,7 +88,20 @@ export default function Map() {
                     </StyledCookieContainer>
                 </Wrapper>
             }
-            {mapCookiesAllowed && <StyledMapElement id="mapContainer" ref={element}></StyledMapElement>}
+            {mapCookiesAllowed &&
+                <StyledMapElement
+                    annotations={hotelsPreview.filter(hotel => hotel.coordinates)?.map(hotel => {
+                        return {
+                            id: hotel.id,
+                            name: hotel.name,
+                            coordinates: { lat: hotel.coordinates.lat, long: hotel.coordinates.long },
+                            color: getVacationTypeColor(hotel.vacationType) || Color.Background,
+                            clusteringIdentifier: hotel.country,
+                        }
+                    })}
+                    onAnnotationClick={id => setSelectedHotel(id)}
+                />
+            }
 
             {selectedHotelContent &&
                 <StyledHotelCardContainer>
