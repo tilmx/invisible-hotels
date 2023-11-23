b5908e84a148ccde93d9b458afb63a7f8f8e944b
Tilman Frick <git@tilman.io>
2023-11-23 01:15:22 +0100

    refactor: simplify tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,24 +3,38 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Color, Theme } from '../components/tokens/colors';
 import { siteTitle } from '../data/site';
-import { TrackingProvider } from '../utils/tracking-provider';
+import Script from 'next/script';
 
 function App({ Component, pageProps }: AppProps) {
+    const trackingEnabled = process.env.NODE_ENV === 'production' &&
+        (!process.env.NEXT_PUBLIC_VERCEL_ENV ||
+            process.env.NEXT_PUBLIC_VERCEL_ENV === 'production');
+
     return (
-        <TrackingProvider>
-            <Theme>
-                <Head>
-                    <link rel="icon" href="/favicon.png" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
-                    <meta name="language" content="en" />
-                    <meta name="keywords" content="Hotels, Hotel, Travel, Boutique Hotels, Design Hotels, Hotel Tips, Hotel Collection, Hotel Recommendations" />
-                    <meta property="og:site_name" content={siteTitle} />
-                    <meta property="og:type" content="website" />
-                </Head>
-                <Global styles={css`body { margin: 0; background: ${Color.Background}; color: ${Color.Text}; }`} />
-                <Component {...pageProps} />
-            </Theme>
-        </TrackingProvider>
+        <Theme>
+            <Head>
+                <link rel="icon" href="/favicon.png" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+                <meta name="language" content="en" />
+                <meta name="keywords" content="Hotels, Hotel, Travel, Boutique Hotels, Design Hotels, Hotel Tips, Hotel Collection, Hotel Recommendations" />
+                <meta property="og:site_name" content={siteTitle} />
+                <meta property="og:type" content="website" />
+            </Head>
+            {trackingEnabled &&
+                <Script
+                    defer
+                    type="text/javascript"
+                    src="https://api.pirsch.io/pirsch-extended.js"
+                    id="pirschextendedjs"
+                    data-code="lTp1KC9kQJ72klGFeYBab8pf59w9VPaW"
+                    onLoad={() => {
+                        console.log('loaddedd')
+                    }}
+                />
+            }
+            <Global styles={css`body { margin: 0; background: ${Color.Background}; color: ${Color.Text}; }`} />
+            <Component {...pageProps} />
+        </Theme>
     );
 }
 
