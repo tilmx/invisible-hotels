0123ad025373d16e5aafb0e29afaa271357a726b
Tilman Frick <git@tilman.io>
2024-10-03 16:37:20 +0200

    refactor: migrate links

--- components/utils/link.tsx
+++ components/utils/link.tsx
@@ -1,45 +1,21 @@
-import styled from '@emotion/styled';
 import NextLink from 'next/link';
 import { CSSProperties, FunctionComponent, HTMLAttributeAnchorTarget, ReactNode } from 'react';
-
-const StyledUnstyledLink = styled(NextLink)`
-    color: unset;
-    text-decoration: unset;
-`;
+import styles from './link.module.scss';
+import clsx from 'clsx';
 
 export const UnstyledLink: FunctionComponent<{ href: string; target?: HTMLAttributeAnchorTarget; children?: ReactNode; className?: string; description?: string; style?: CSSProperties }> = props => {
     return (
-        <StyledUnstyledLink aria-label={props.description} href={props.href} target={props.target} className={props.className} style={props.style}>
+        <NextLink className={clsx(styles.unstyledLink, props.className)} aria-label={props.description} href={props.href} target={props.target} style={props.style}>
             {props.children}
-        </StyledUnstyledLink>
+        </NextLink>
     );
 };
 
-const StyledLink = styled(StyledUnstyledLink)`
-    opacity: .6;
-    cursor: pointer;
-    padding: var(--size-xxs) var(--size-s);
-    margin: calc(-1 * var(--size-xxs)) calc(-1 * var(--size-s));
-    border-radius: var(--size-m);
-
-    @media (hover: hover) {
-        :hover {
-            background: var(--color-text10);
-            opacity: .8;
-        }
-    }
-
-    :active {
-        background: var(--color-text20);
-        opacity: .8;
-    }
-`;
-
 export const Link: FunctionComponent<{ description?: string; href: string; target?: HTMLAttributeAnchorTarget; children?: ReactNode; className?: string; }> = props => {
     return (
-        <StyledLink aria-label={props.description} href={props.href} target={props.target} className={props.className}>
+        <NextLink className={clsx(styles.styledLink, props.className)} aria-label={props.description} href={props.href} target={props.target} >
             {props.children}
-        </StyledLink>
+        </NextLink>
     );
 };
 
