37ad0222e76157861aa4334990cbf655f87939e1
Tilman Frick <git@tilman.io>
2024-10-03 14:36:47 +0200

    refactor: migrate wrapper component

--- components/wrapper.tsx
+++ components/wrapper.tsx
@@ -1,27 +1,11 @@
-import styled from '@emotion/styled'
-import { Breakpoint } from './tokens/breakpoint';
+import clsx from 'clsx';
 import { CSSProperties, FunctionComponent, ReactNode } from 'react';
-
-const StyledWrapper = styled.div<{ wide?: boolean; }>`
-    padding: 0 var(--size-xxl);
-    margin: 0 auto;
-    max-width: ${props => props.wide ? '1280px' : '1080px'};
-
-    ${Breakpoint.Tablet} {
-        padding-left: var(--size-l);
-        padding-right: var(--size-l);
-    }
-
-    ${Breakpoint.Mobile} {
-        padding-left: var(--size-m);
-        padding-right: var(--size-m);
-    }
-`
+import styles from './wrapper.module.scss';
 
 export const Wrapper: FunctionComponent<{ children: ReactNode; wide?: boolean; style?: CSSProperties; className?: string; }> = props => {
     return (
-        <StyledWrapper wide={props.wide} style={props.style} className={props.className}>
+        <div className={clsx(styles.wrapper, props.wide && styles.wide, props.className)} style={props.style}>
             {props.children}
-        </StyledWrapper>
+        </div>
     )
 }
