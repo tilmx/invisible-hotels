5b2e68bd21f8801b2600bd93cc52f566638833ed
Tilman Frick <git@tilman.io>
2024-10-03 15:56:14 +0200

    refactor: migrate hotel list

--- components/hotel-list.tsx
+++ components/hotel-list.tsx
@@ -1,4 +1,3 @@
-import styled from '@emotion/styled';
 import hotelsPreview from '../data/hotels-preview.json';
 import { FunctionComponent, useEffect } from 'react';
 import { checkIfFavoritesStored, track } from '../utils';
@@ -6,42 +5,12 @@ import { PlaceholderCard } from './placeholder-card';
 import { Text, TextSize } from './text';
 import { Wrapper } from './wrapper';
 import { HotelCard } from './hotel-card';
-import { Breakpoint } from './tokens/breakpoint';
 import { Color } from './tokens/colors';
 import { useFilterStore } from '../store/filter';
 import { useFavoriteStore } from '../store/favorites';
 import { Filter } from './filter';
 import { HotelListWrapper } from './hotel-list-wrapper';
-
-const StyledContainer = styled.div`
-    margin-top: var(--size-xl);
-    padding-top: var(--size-xl);
-
-    ${Breakpoint.Tablet} {
-        margin-top: var(--size-xxl);
-        grid-template-columns: 1fr;
-    }
-
-    ${Breakpoint.Mobile} {
-        margin-top: var(--size-xl);
-    }
-`;
-
-const StyledLabel = styled(Text)`
-    z-index: 11;
-    position: relative;
-    transform: translate3d(0,0,0); 
-    margin-bottom: var(--size-xs);
-`;
-
-const StyledHotelListWrapper = styled(HotelListWrapper)`
-    margin-top: var(--size-xxxl);
-
-    ${Breakpoint.TabletSmall} {
-        grid-template-columns: 1fr;
-        margin-top: var(--size-xl);
-    }
-`;
+import styles from './hotel-list.module.scss';
 
 export const HotelList: FunctionComponent = () => {
 
@@ -74,12 +43,12 @@ export const HotelList: FunctionComponent = () => {
     const labelText = `${(vacationTypeFilter || countryFilter) ? 'Filtered' : 'Filter all'} ${filteredHotels.length.toString()} hotel${plural ? 's' : ''} & apartment${plural ? 's' : ''}`;
 
     return (
-        <StyledContainer id="hotel-list">
+        <div className={styles.hotelList} id="hotel-list">
             <Wrapper>
-                <StyledLabel size={TextSize.Regular} color={Color.Text60}>{labelText}</StyledLabel>
+                <Text className={styles.label} size={TextSize.Regular} color={Color.Text60}>{labelText}</Text>
             </Wrapper>
             <Filter />
-            <StyledHotelListWrapper>
+            <HotelListWrapper className={styles.hotelListWrapper}>
                 {filteredHotels.map((hotel, i) => {
                     const isFavorite = favorites.includes(hotel.id);
                     return (
@@ -97,7 +66,7 @@ export const HotelList: FunctionComponent = () => {
                 }
                 )}
                 <PlaceholderCard emptyState={isEmpty} />
-            </StyledHotelListWrapper>
-        </StyledContainer>
+            </HotelListWrapper>
+        </div>
     )
 }
