c1137af23c5da5f34f437e75c95c29d003b164c3
Tilman Frick <git@tilman.io>
2024-10-03 14:04:15 +0200

    refactor: migrate placeholder card

--- components/placeholder-card.tsx
+++ components/placeholder-card.tsx
@@ -1,10 +1,10 @@
-import styled from '@emotion/styled';
 import { FunctionComponent, ReactNode } from 'react';
-import { Breakpoint } from './tokens/breakpoint';
 import { Text, TextSize } from './text';
 import { UnstyledLink } from './utils/link';
 import { Button } from './button';
 import { SendIcon } from 'lucide-react';
+import styles from './placeholder-card.module.scss';
+import clsx from 'clsx';
 
 interface PlaceholderCardProps {
     children?: ReactNode;
@@ -12,36 +12,13 @@ interface PlaceholderCardProps {
     emptyState: boolean;
 }
 
-const StyledCard = styled.div<{ emptyState: boolean }>`
-    border-radius: var(--size-m);
-    padding: var(--size-l);
-    display: flex;
-    flex-direction: column;
-    gap: var(--size-m);
-    box-shadow: inset 0 0 0 2px var(--color-text10);
-    color: var(--color-text60);
-    justify-content: center;
-    align-items: center;
-
-    ${props => props.emptyState && `
-        max-width: 400px;
-        place-self: center;
-        grid-column: 1/4;
-    `}
-
-    ${Breakpoint.Mobile} {
-        padding: var(--size-m);
-        border-radius: var(--size-s);
-    }
-`;
-
 export const PlaceholderCard: FunctionComponent<PlaceholderCardProps> = props => {
     return (
-        <StyledCard emptyState={props.emptyState} className={props.className}>
+        <div className={clsx(styles.placeholderCard, props.emptyState && styles.emptyState, props.className)}>
             <Text center size={TextSize.Regular}>{props.emptyState ? "It looks like we haven't been in such a place. Any tips?" : "You have a secret hotel tip for us or some feedback? Let us know!"}</Text>
             <UnstyledLink href={`mailto:${process.env.NEXT_PUBLIC_CONTACT_EMAIL}?subject=${encodeURI('I have a secret hotel tip for you!')}&body=${encodeURI('Hey Annika and Tilman! \n\n I have a super secret hotel tip for you — here it is:')}`}>
                 <Button iconLeft={<SendIcon />} small secondary>Send E-Mail</Button>
             </UnstyledLink>
-        </StyledCard>
+        </div>
     )
 }
