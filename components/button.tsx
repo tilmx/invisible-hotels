20797df5ab995196f5fe33ca7652e3f3fd53fd9f
Tilman Frick <git@tilman.io>
2024-10-03 13:37:36 +0200

    refactor: migrate button

--- components/button.tsx
+++ components/button.tsx
@@ -1,47 +1,22 @@
-import styled from "@emotion/styled";
 import { FunctionComponent, MouseEventHandler, ReactNode } from "react";
 import { Text } from "./text";
-import { Color } from "./tokens/colors";
-import { Breakpoint } from "./tokens/breakpoint";
-
-const StyledButton = styled.div<{ secondary?: boolean; small?: boolean; }>`
-    background: ${props => props.secondary ? Color.Text10 : Color.Text};
-    color: ${props => props.secondary ? Color.Text60 : Color.Background};
-    padding: ${props => props.small ? `var(--size-xxs) var(--size-s)` : `var(--size-s) var(--size-m)`};
-    border-radius: var(--size-l);
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-	gap: var(--size-xxs);
-
-    :active {
-        background: ${Color.Text80};
-    }
-
-    @media (hover: hover) {
-        :hover {
-            background: ${props => props.secondary ? Color.Text20 : Color.Text80};
-            color: ${props => props.secondary ? Color.Text : Color.Background};
-        }
-    }
-
-    ${Breakpoint.Mobile} {
-        svg {
-            height: 20px;
-            width: auto;
-        }
-    }
-`;
+import styles from './button.module.scss';
+import clsx from "clsx";
 
 export const Button: FunctionComponent<{ iconLeft?: ReactNode; iconRight?: ReactNode; small?: boolean; secondary?: boolean; children?: ReactNode; onClick?: MouseEventHandler; }> = props => {
     return (
-        <StyledButton small={props.small} secondary={props.secondary} onClick={props.onClick}>
+        <div className={clsx(
+            styles.button,
+            props.small && styles.small,
+            props.secondary && styles.secondary
+        )}
+            onClick={props.onClick}
+        >
             {props.iconLeft}
             <Text center>
                 {props.children}
             </Text>
             {props.iconRight}
-        </StyledButton>
+        </div>
     )
 }
