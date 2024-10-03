48d6b5247e015268d6b3c4ab1973e84c1d9dee2f
Tilman Frick <git@tilman.io>
2024-10-03 14:09:16 +0200

    refactor: migrate scroll indicator

--- components/scroll-indicator.tsx
+++ components/scroll-indicator.tsx
@@ -1,43 +1,7 @@
-import styled from '@emotion/styled';
 import { ArrowDown } from 'lucide-react';
 import { FunctionComponent, MouseEventHandler, useEffect, useState } from 'react';
-import { Breakpoint } from './tokens/breakpoint';
-
-const StyledIndicator = styled.div<{ visible: boolean; }>`
-    position: fixed;
-    left: 50%;
-    bottom: var(--size-xl);
-    height: var(--size-xl);
-    width: var(--size-xl);
-    border-radius: 50%;
-    background: var(--color-yellow);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition: transform .2s, opacity .2s;
-    transform: translateX(-50%);
-    box-shadow: 0 0 var(--size-xxl) var(--color-shadow);
-    cursor: pointer;
-
-    ${Breakpoint.Mobile} {
-        bottom: var(--size-l);
-    }
-
-    :active {
-        transform: translateX(-50%) scale(1.1);
-    }
-
-    @media (hover: hover) {
-        :hover {
-            transform: translateX(-50%) scale(1.1);
-        }
-    }
-
-    ${props => !props.visible && `
-        transform: translateX(-50%) scale(0);
-        opacity: 0;
-    `}
-`;
+import styles from './scroll-indicator.module.scss';
+import clsx from 'clsx';
 
 export const ScrollIndicator: FunctionComponent<{ onClick: MouseEventHandler }> = props => {
     const [visible, setVisible] = useState(false);
@@ -55,8 +19,8 @@ export const ScrollIndicator: FunctionComponent<{ onClick: MouseEventHandler }> 
     }
 
     return (
-        <StyledIndicator onClick={props.onClick} visible={visible}>
+        <div className={clsx(styles.scrollIndicator, visible && styles.visible)} onClick={props.onClick}>
             <ArrowDown />
-        </StyledIndicator>
+        </div>
     )
 }
