aa1ae8fea70639610c8d97ea64506671721a81be
Tilman Frick <git@tilman.io>
2024-10-03 14:12:41 +0200

    refactor: migrate intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,21 +1,11 @@
-import styled from '@emotion/styled'
 import { FunctionComponent } from 'react';
 import { Color } from './tokens/colors';
 import { Text, TextSize } from './text';
 import { AccentStyle, AccentedText } from './accented-text';
-
-const StyledText = styled(Text)`
-	max-width: 840px;
-	width: 95%;
-`;
-
-const StyledHeadline = styled(Text)`
-	display: inline;
-	font-size: .85em;
-`;
+import styles from './intro.module.scss';
 
 export const Intro: FunctionComponent = () => {
 	return (
-		<StyledText size={TextSize.ExtraLarge} serif><StyledHeadline bold size={TextSize.Unset}>Invisible Hotels</StyledHeadline> are all the <AccentedText italic color={Color.Blue} accentStyle={AccentStyle.Scribbled}>lovely</AccentedText>, <AccentedText italic color={Color.Green} accentStyle={AccentStyle.Circled}>minimalistic</AccentedText>, and <AccentedText italic color={Color.Yellow} accentStyle={AccentStyle.Underlined}>fancy</AccentedText> hotels & apartments where we stayed already — or would love to.</StyledText>
+		<Text className={styles.intro} size={TextSize.ExtraLarge} serif><Text className={styles.headline} bold size={TextSize.Unset}>Invisible Hotels</Text> are all the <AccentedText italic color={Color.Blue} accentStyle={AccentStyle.Scribbled}>lovely</AccentedText>, <AccentedText italic color={Color.Green} accentStyle={AccentStyle.Circled}>minimalistic</AccentedText>, and <AccentedText italic color={Color.Yellow} accentStyle={AccentStyle.Underlined}>fancy</AccentedText> hotels & apartments where we stayed already — or would love to.</Text>
 	)
 }
