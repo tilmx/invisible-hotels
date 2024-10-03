5c67ce70166baaf6c5bacccb71af222115acf3f4
Tilman Frick <git@tilman.io>
2024-10-03 14:39:43 +0200

    refactor: migrate share component

--- components/share.tsx
+++ components/share.tsx
@@ -1,26 +1,12 @@
 import { FunctionComponent, useEffect, useState } from "react";
 import { Button } from "./button";
-import { Flex, JustifyContent } from "./utils/flex";
 import { ShareIcon } from "lucide-react";
-import styled from "@emotion/styled";
 import { siteDescription, siteTitle } from "../data/site";
 import { Text, TextSize } from "./text";
 import { Color } from "./tokens/colors";
 import { AccentStyle, AccentedText } from "./accented-text";
 import { Wrapper } from "./wrapper";
-
-const StyledWrapper = styled(Wrapper)`
-    max-width: 480px;
-    margin: 0 auto;
-    padding-top: var(--size-xxxxl);
-    display: flex;
-    flex-direction: column;
-    gap: var(--size-s);
-`;
-
-const StyledFlex = styled(Flex)`
-    margin-top: var(--size-m);
-`;
+import styles from './share.module.scss';
 
 export const Share: FunctionComponent = () => {
     const [shareAvailable, setShareAvailable] = useState(false);
@@ -42,7 +28,7 @@ export const Share: FunctionComponent = () => {
     }
 
     return (
-        <StyledWrapper>
+        <Wrapper className={styles.share}>
             <Text size={TextSize.SuperLarge} center bold>
                 <AccentedText color={Color.Text80} accentStyle={AccentStyle.Circled}>
                     {siteTitle}
@@ -50,12 +36,12 @@ export const Share: FunctionComponent = () => {
             </Text>
             <Text size={TextSize.Large} serif center color={Color.Text60}>{siteDescription}</Text>
             {shareAvailable ?
-                <StyledFlex justifyContent={JustifyContent.Center}>
+                <div className={styles.shareContainer}>
                     <Button iconLeft={<ShareIcon />} onClick={() => share()}>Share Website</Button>
-                </StyledFlex>
+                </div>
                 :
                 <div />
             }
-        </StyledWrapper>
+        </Wrapper>
     )
 }
