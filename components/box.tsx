41dcd60ff80a335a1144e26e2207479d7e157315
Tilman Frick <git@tilman.io>
2024-10-03 13:37:28 +0200

    refactor: migrate box

--- components/box.tsx
+++ components/box.tsx
@@ -1,34 +1,17 @@
-import styled from '@emotion/styled'
 import { Text, TextSize } from './text';
 import { FunctionComponent, ReactNode } from 'react';
 import { MapPinIcon } from 'lucide-react';
 import { Link } from './utils/link';
-
-const StyledMessage = styled.div`
-    padding: var(--size-l);
-    max-width: 400px;
-    margin: 0 auto;
-    border-radius: var(--size-m);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: var(--size-s);
-
-    > svg {
-        display: block;
-        width: var(--size-l);
-        height: var(--size-l);
-    }
-`;
+import styles from './box.module.scss';
 
 export const Box: FunctionComponent<{ title: string; description: string; children?: ReactNode; }> = props => {
     return (
-        <StyledMessage>
+        <div className={styles.box}>
             <MapPinIcon />
             <Text center size={TextSize.Large} bold>{props.title}</Text>
             <Text center size={TextSize.Regular}>{props.description}</Text>
             <Text center size={TextSize.Small}>Find out more in our <Link href="legal/privacy-policy">privacy policy</Link>.</Text>
             {props.children}
-        </StyledMessage>
+        </div>
     )
 }
