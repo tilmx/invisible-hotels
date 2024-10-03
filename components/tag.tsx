c86f5d57b84673574611ff08e6dff5ff6f14b937
Tilman Frick <git@tilman.io>
2024-10-03 14:22:54 +0200

    refactor: migrate tag component

--- components/tag.tsx
+++ components/tag.tsx
@@ -1,37 +1,22 @@
-import styled from '@emotion/styled'
 import { Text } from './text';
 import { FunctionComponent, MouseEventHandler, ReactNode } from 'react';
-import { Breakpoint } from './tokens/breakpoint';
-
-const StyledTag = styled(Text) <{ hasIcon?: boolean; hasLabel?: boolean; }>`
-	border: 2px solid currentColor;
-	padding: var(--size-xxs) var(--size-s);
-	${props => props.hasIcon && `padding-left: var(--size-xs);`}
-	${props => !props.hasLabel && `padding-right: var(--size-xs);`}
-	border-radius: 24px;
-	display: flex;
-	gap: var(--size-xxs);
-	align-items: center;
-
-	${Breakpoint.Mobile} {
-		padding: var(--size-special6) var(--size-xs);
-		${props => props.hasIcon && `padding-left: var(--size-xxs);`}
-		${props => !props.hasLabel && `padding-right: var(--size-xxs);`}
-		border-width: 1.5px;
-		gap: var(--size-xxxs);
-		svg, img {
-			width: 20px;
-			height: 20px;
-		}
-	}
-`;
+import styles from './tag.module.scss';
+import clsx from 'clsx';
 
 export const Tag: FunctionComponent<{ icon?: ReactNode; label?: string; children?: ReactNode; onClick?: MouseEventHandler; className?: string; }> = props => {
 	return (
-		<StyledTag hasIcon={typeof props.icon !== 'undefined'} hasLabel={typeof props.label !== 'undefined'} onClick={props.onClick} className={props.className}>
+		<Text
+			className={clsx(
+				styles.tag,
+				(typeof props.icon !== 'undefined') && styles.hasIcon,
+				(typeof props.label !== 'undefined') && styles.hasLabel,
+				props.className
+			)}
+			onClick={props.onClick}
+		>
 			{props.icon}
 			{props.label}
 			{props.children}
-		</StyledTag>
+		</Text>
 	)
 }
